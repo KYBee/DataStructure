74d9d466e29a6b82633343603948a9c49d27da4a
KYBee <kyb3634@naver.com>
2021-04-05 18:35:50 +0900

    start implementing get_path function

--- assignment2/2.py
+++ assignment2/2.py
@@ -1,15 +1,17 @@
 class Stack:
-
     def __init__(self):
         self.items = []
         self.top = -1
+        self.call = 0
 
     def push(self, val):
         self.items.append(val)
+        self.call += 1
 
     def pop(self):
         try:
             return self.items.pop()
+            self.call += 1
         except IndexError:
             print("Stack is empty")
 
@@ -31,6 +33,9 @@ class Stack:
         except IndexError:
             print("Stack is empty")
 
+    def how_many(self):
+        return self.call
+
 class Point:
     def __init__(self, row, col):
         #openned = 1, closed = 0
@@ -39,62 +44,81 @@ class Point:
         self.bottom = 1
         self.left = 1
         self.__location = (row, col)
+        self.__checked = False
     
+    def down(self):
+        return (self.__location[0] + 1, self.__location[1])
+
+    def up(self):
+        return (self.__location[0] - 1, self.__location[1])
+
+    def left(self):
+        return (self.__location[0], self.__location[1] - 1)
+
+    def right(self):
+        return (self.__location[0], self.__location[1] + 1)
+
+
     def get_location(self):
         return self.__location
     
     def set_location(self, location):
         self.__location = location
 
+    def get_checked(self):
+        return self.__checked
+    
+    def set_checked(self):
+        return self.__checked
+
     def __str__(self):
-        return "(%2d %2d)" % self.__location
+        return str((self.__location[0], self.__location[1]))
 
 class Maze:
     maze_row = 12
     maze_col = 9
     commands = (
-        #row, col, vertical, horizontal
+        #vertical, horizontal
         #in vertical -1 for top closed, 1 for bottom closed, 2 for all closed
         #in horizontal -1 for left closed, 1 for right closed, 2 for all closed
-
-        #vertical, horizontal
         ((-1, -1), (2, 0), (2, 0), (-1, 1), (2, -1), (2, 0), (-1, 0), (-1, 1), (-1, 2)),
         ((1, -1), (2, 0), (-1, 1), (0, 2), (-1, -1), (2, 0), (0, 1), (0, 2), (0, 2)),
         ((-1, -1), (2, 0), (0, 1), (1, -1), (1, 1), (-1, -1), (1, 1), (0, 2), (0, 2)),
-        ((0, 2), (-1, 2), (1, 2), (-1, 2), (-1, 2), (1, 2), (-1, -1), (1, 0), (0, 1)),
+        ((0, 2), (-1, 2), (1, 2), (-1, 2), (-1, 2), (1, 2), (-1, -1), (1, 0), (1, 1)),
         ((0, 2), (0, 2), (-1, -1), (0, 1), (1, -1), (2, 0), (1, 1), (-1, -1), (0, 1)),
-        ((0, -1), (1, 1), (0, 2), (0, -1), (2, 0), (2, 1), (2, -1), (1, 1), (0, -1)),
+        ((0, -1), (1, 1), (0, 2), (0, -1), (2, 0), (2, 1), (2, -1), (1, 1), (0, 2)),
         ((1, -1), (2, 0), (1, 1), (0, 2), (-1, -1), (2, 0), (2, 0), (-1, 0), (0, 1)),
         ((-1, 2), (-1, -1), (2, 0), (1, 1), (0, 2), (-1, -1), (2, 0), (1, 1), (1, 2)),
-        ((0, 2), (1, -1), (2, 0), (-1, 0), (1, 1), (1, -1), (-1, 0), (2, 0), (-1, 0)),
+        ((0, 2), (1, -1), (2, 0), (-1, 0), (1, 1), (1, -1), (-1, 0), (2, 0), (-1, 1)),
         ((0, -1), (2, 0), (-1, 1), (0, -1), (-1, 1), (-1, 2), (1, 2), (-1, 2), (0, 2)),
         ((1, -1), (-1, 1), (0, 2), (0, 2), (0, 2), (0, -1), (2, 0), (1, 1), (0, 2)),
         ((2, -1), (1, 1), (1, -1), (1, 1), (1, -1), (1, 1), (2, -1), (2, 0), (1, 1)),
     )
 
     def __init__(self):
+        #make empty 2 dimension list
         self.composition = [[0 for col in range(self.maze_col)] for row in range(self.maze_row)]
         
+        #assign Point class to each 2 dimension list
         for row in range(self.maze_row):
             for col in range(self.maze_col):
                 self.composition[row][col] = Point(row, col)
 
-        for col in range(self.maze_col):
-            self.composition[0][col].top = 0
-            self.composition[-1][col].bottom = 0
-        for row in range(self.maze_row):
-            self.composition[row][0].left = 0
-            self.composition[row][-1].right = 0
-
+        #setting Point class's information
         for row in range(self.maze_row):
             for col in range(self.maze_col):
                 self.set_point(row, col, self.commands[row][col])
+
+        #setting start and end Point
+        self.start_point = self.composition[0][0]
+        self.end_point = self.composition[-1][-1]
         
 
     def set_point(self, row, col, command):
         vertical = command[0]
         horizontal = command[1]
 
+        # closed top = -1, closed bottom = 1, closed both side = 2
         if vertical == -1:
             self.composition[row][col].top = 0
         elif vertical == 1:
@@ -103,6 +127,7 @@ class Maze:
             self.composition[row][col].top = 0
             self.composition[row][col].bottom = 0
 
+        # closed left = -1, closed right = 1, closed both side = 2
         if horizontal == -1:
             self.composition[row][col].left = 0
         elif horizontal == 1:
@@ -111,8 +136,41 @@ class Maze:
             self.composition[row][col].left = 0
             self.composition[row][col].right = 0
 
+    #get path
+    def get_path(self):
+        #path_stack initialize
+        path_stack = Stack()
 
+        current = self.start_point
+        recent = self.start_point
+
+        row, col = 0, 0
+        visited = []
+
+        # current가 end_point에 도달할 때 까지
+        while current.get_location() != self.end_point.get_location():
+            # top bottom left right
+            temp = [0, 0, 0, 0]
+            # 만약 해당 Point에서 열려있고 가장 최근에 방문했던 path가 아닌 것들을 temp리스트에 넣음
+            # temp 리스트의 길이가 0이면 막혀있음
+            # temp 리스트의 길이가 1이면 포인트를 다음 포인트로 변경하여 다음 while 문 진행
+            # temp 리스트의 길이가 2이면 해당 포인트를 stack에 넣고 둘 중 하나를 선택한 후 다음 포인트를 진행
+            if current.top and current.up() != recent.get_location():
+                temp[0] = 1
+            elif current.bottom and current.down() != recent.get_location():
+                temp[1] = 1
+            elif current.left and current.left() != recent.get_location():
+                temp[2] = 1
+            elif current.right and current.right() != recent.get_location():
+                temp[3] = 1
+            
+            
+               
+        
+        
+    # printing Maze itself
     def print_maze(self):
+
         #print top line
         for col in range(self.maze_col):
             print("%3s" % "_______", end="")
@@ -157,7 +215,6 @@ class Maze:
                 else:
                     print(" ", end="")
                     
-                
                 #print right
                 if not(self.composition[row][col].right):
                     print("|", end = "")
@@ -166,11 +223,14 @@ class Maze:
                         print("_", end="")
                     else:
                         print(" ", end="")
-            
             print()
-
 
 
 
 a = Maze()
 a.print_maze()
+
+
+b = Point(1, 1)
+print(b.down())
+print(b)

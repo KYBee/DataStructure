02b42bdad32c6ef1c026a76ee75da16e54bacea1
KYBee <kyb3634@naver.com>
2021-04-17 15:26:38 +0900

    sorting by python inherited method

--- assignment3/1.py
+++ assignment3/1.py
@@ -1,17 +1,30 @@
 import random
 import string
+
+#Define Data Size
+DATA_SIZE = 10
+
 #학번 8자리 출력
-
 #O(n)
-random_id = random.sample(range(10000000, 100000000), 10)
-print(random_id)
+random_id = random.sample(range(10000000, 100000000), DATA_SIZE)
 
-final_list = list()
-
+original_data = list()
 for r in random_id:
-    #TODO 이름, 전화번호
     name = ''.join(random.choice(string.ascii_uppercase) for _ in range(8))
     phone_number = "010" + str(format(random.randint(0, 100000000), "08")) 
-    final_list.append([r, name, phone_number])
+    original_data.append([r, name, phone_number])
 
-print(final_list)
+#Producing original data
+print("Original Data")
+for student in original_data:
+    print(student)
+
+
+#Sorting by Python inherited method
+#Sorted는 정렬 대상이 되는 Data를 받아 정렬한 리스트를 리턴하기 때문에 기존의 데이터는 변화가 없다.
+python_inherited_sorting = sorted(original_data, key=lambda name: name[1])
+
+
+print('\nPython inherited sorting')
+for student in python_inherited_sorting:
+    print(student)

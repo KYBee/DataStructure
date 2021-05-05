3e2fe3f3370c63c6ff860cd3ef5aa640143aef32
yb5464 <kyb3634@naver.com>
2021-05-05 20:49:15 +0900

    1.py define sorting and insert

--- assignment4/1.py
+++ assignment4/1.py
@@ -5,11 +5,11 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.head = None
+        head = Node(None)
+        self.head = head
     
     def getNode(self, pos):
         #이때 넘어오는 pos는 사용자가 입력한 pos 값에서 -1 해준 값임
-
         if pos < 0:
             return None
         
@@ -20,17 +20,15 @@ class LinkedList:
         
         return node
     
-    def insert(self, pos, elem):
-        before = self.getNode(pos - 1)
+    def insert(self, elem):
+        before = self.head
 
-        if before is None:
-            #텅 빈 연결 리스트 이므로
-            self.head = Node(elem, self.head)
-            #self.head = Node(elem)
-        
-        else:
-            before.link = Node(elem, before.link)
-        
+        while before.link != None:
+            if before.link.data[0] > elem[0]:
+                break
+            before = before.link
+
+        before.link = Node(elem, before.link)
     
     def delete(self, pos):
         before = self.getNode(pos - 1)
@@ -43,11 +41,29 @@ class LinkedList:
         del removed
     
     def display(self):
-        if self.head == None:
-            print("empty")
         node = self.head
-        while node != None:
-            print(node.data)
+        while node.link != None:
+            print(node.link.data)
             node = node.link
 
 
+randdict = LinkedList()
+
+with open('randdict_utf8.TXT', 'r') as f:
+    
+    #for debugging
+    i = 0
+
+    for line in f:
+        line = line.strip().split(":")
+        line = list(map(lambda x: x.strip(), line))
+
+        #TODO 올바르지 않은 형식의 사전 데이터 삭제
+        randdict.insert(line)
+
+        if i == 3:
+            break
+        else:
+            i += 1
+
+    randdict.display()

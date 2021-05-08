bb8b765479c4d593398ab60208586edecaec9e8b
KYBee <kyb3634@naver.com>
2021-05-08 22:57:31 +0900

    fin 2.py

--- assignment4/2.py
+++ assignment4/2.py
@@ -4,75 +4,67 @@ import string
 class Node:
     def __init__(self, data, link=None):
         self.data = data
-        self.link = link
+        self.num_link = link
+        self.name_link = link
 
 class LinkedList:
     def __init__(self):
-        self.head = None
+        self.num_head = Node(None)
+        self.name_head = Node(None)
     
-    def getNode(self, pos):
-        if pos < 0:
-            return None
-        
-        node = self.head
-        while pos > 0 and node != None:
-            node = node.link
-            pos -= 1
-        
-        return node
-    
-    def insert(self, elem):
-        #before = self.getNode(pos - 1)
+    def insert(self, student):
+        student = Node(student)
 
-        if self.head is None:
-            #텅 빈 연결 리스트 이므로
-            self.head = Node(elem, self.head)
-            #self.head = Node(elem)
-        
+        before = self.num_head
+        while before.num_link != None:
+            if before.num_link.data[0] > student.data[0]:
+                break
+            before = before.num_link
+        before.num_link, student.num_link = student, before.num_link
+
+        before = self.name_head
+        while before.name_link != None:
+            if before.name_link.data[1] > student.data[1]:
+                break
+            before = before.name_link
+        before.name_link, student.name_link = student, before.name_link
+
+    def display(self, op):
+        if op == "1":
+            cnt = 0
+            node = self.num_head
+            while node.num_link != None:
+                cnt += 1
+                if cnt % 100 == 0:
+                    print("%5d번 학생: 학번 -> %s, 이름 -> %s, 전화번호 -> %s" % (cnt, node.num_link.data[0], node.num_link.data[1], node.num_link.data[2]))
+                node = node.num_link
+        elif op == "2":
+            cnt = 0
+            node = self.name_head
+            while node.name_link != None:
+                cnt += 1
+                if cnt % 100 == 0:
+                    print("%5d번 학생: 학번 -> %s, 이름 -> %s, 전화번호 -> %s" % (cnt, node.name_link.data[0], node.name_link.data[1], node.name_link.data[2]))
+                node = node.name_link
         else:
-            before = self.head
-            while before.link is not None:
-                print('for debugging')
-                print(before.data[0], elem[0])
-                if before.data[0] > elem[0]:
-                    break
-                before = before.link
+            print("잘못된 입력입니다.")
+            return
 
-            print("final before", before.data)
-            before.link = Node(elem, before)
-        
-    
-    def delete(self, pos):
-        before = self.getNode(pos - 1)
-        if before is None:
-            removed = self.head
-            self.head = self.head.link
-        else:
-            removed = before.link
-            before.link = removed.link
-        del removed
-    
-    def display(self):
-        if self.head == None:
-            print("empty")
-        node = self.head
-        while node != None:
-            print(node.data)
-            node = node.link
+DATA_SIZE = 10000
+student_data = LinkedList()
 
-DATA_SIZE = 10
-#Making Original Data
-#학번 8자리 출력
-#O(n)
 random_id = random.sample(range(20130000, 20220000), DATA_SIZE)
-original_data = list()
 for r in random_id:
     name_len = random.randint(1, 10)
     name = ''.join(random.choice(string.ascii_uppercase) for _ in range(name_len))
     phone_number = "010" + str(format(random.randint(0, 100000000), "08")) 
-    original_data.append([r, name, phone_number])
+    student_data.insert([r, name, phone_number])
 
-
-#for debugging
-for student in original_data:
-    print(student)
+while True:
+    print("\n(1) 학번순\n(2) 이름순")
+    op = input("메뉴 선택(0 : 종료) : ")
+    if op == '0':
+        print("종료")
+        break
+    else:
+        student_data.display(op)

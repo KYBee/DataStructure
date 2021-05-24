bfe6a98c37e286bb9332587d14a8fa247d11a8b4
KYBee <kyb3634@naver.com>
2021-05-24 20:03:36 +0900

    define print_result, tree A

--- assignment5/example.py
+++ assignment5/example.py
@@ -1,5 +1,3 @@
-import random
-
 class Node :
     def __init__(self, item, left=None, right=None) :
         self.item = item
@@ -13,77 +11,18 @@ class TreeA :
     def __init__(self):
         self.root = None
 
-    def get_total_count(self, n):
-        if n is None: return 0
-        return 1 + self.get_total_count(n.left) + self.get_total_count(n.right)
-
     @classmethod
     def print(cls, n):
         if n != None :
             cls.print(n.left)
-            print(n.item)
+            print(n.item, end=" ")
             cls.print(n.right)
 
     @classmethod
     def print_pre(cls, n):
-        if n != None :
-            print(n.item, end=" ")
+        if n != None:
+            print(n.item, end="")
             cls.print(n.left)
-            cls.print(n.right)
-
-    def search(self, item):
-        if self.root is None:
-            return None
-        else:
-            return self.__search_node(self.root, item, level = 0)
-
-    def __search_node(self, cur, item, level):
-        if cur is None:
-            return None, None
-        if cur.item[0] == item:
-            return cur, level
-        else:
-            if cur.item[0] >= item:
-                level += 1
-                return self.__search_node(cur.left, item, level)
-            else:
-                level += 1
-                return self.__search_node(cur.right, item, level)
-
-    def insert(self, item):
-        if self.root is None:
-            self.root = Node(item)
-        else:
-            self.__insert_node(self.root, item)
-        
-    def __insert_node(self, cur, item):
-        #head 값이 크면 왼쪽으로
-        if cur.item[0] >= item[0]:
-            if cur.left is not None:
-                self.__insert_node(cur.left, item)
-            else:
-                cur.left = Node(item)
-        #head 값이 작으면 오른쪽으로
-        else:
-            if cur.right is not None:
-                self.__insert_node(cur.right, item)
-            else:
-                cur.right = Node(item)
-        
-    def calc_height(self, n=None):
-        if n is None: return 0
-        return 1 + max(self.calc_height(n.left), self.calc_height(n.right))
-
-
-class TreeB :
-    def __init__(self):
-        self.root = None
-
-    @classmethod
-    def print(cls, n):
-        if n != None :
-            cls.print(n.left)
-            print(n.item, end=" ")
             cls.print(n.right)
 
     def insert(self, item):
@@ -105,58 +44,111 @@ class TreeB :
                 self.__insert_node(cur.right, item)
             else:
                 cur.right = Node(item)
+        
+    def calc_height(self, n=None):
+        if n is None: return 0
+        return 1 + max(self.calc_height(n.left), self.calc_height(n.right))
+    
+
+class TreeB :
+    def __init__(self):
+        self.root = None
+        self.height = -1
+        self.balance = 0
+
+    @classmethod
+    def print(cls, n):
+        if n != None :
+            cls.print(n.left)
+            print(n.item, end=" ")
+            cls.print(n.right)
+
+    @classmethod
+    def print_pre(cls, n):
+        if n != None:
+            print(n.item, end="")
+            cls.print(n.left)
+            cls.print(n.right)
+
+    def insert(self, item):
+        if self.root is None:
+            self.root = Node(item)
+        else:
+            self.__insert_node(self.root, item)
+        
+    def __insert_node(self, cur, item):
+        #head 값이 크면 왼쪽으로
+        if cur.item >= item:
+            if cur.left is not None:
+                self.__insert_node(cur.left, item)
+            else:
+                cur.left = Node(item)
+        #head 값이 작으면 오른쪽으로
+        else:
+            if cur.right is not None:
+                self.__insert_node(cur.right, item)
+            else:
+                cur.right = Node(item)
+            
+        #TODO Do something for balancing
+        self.rebalance()
+
+    def rebalance(self):
+        #TODO if unbalanced -> make it balanced
+        #TODO decide where to rotate
+        pass
+
+    def rrotate(self):
+        A = self.root
+        B = self.root.left
+        T = B.right
+
+        self.root = B
+        B.right = A
+        A.left = T
+
+    def lrotate(self):
+        A = self.root
+        B = self.root.right
+        T = B.left
+
+        self.root = B
+        B.left = A
+        A.right = T
 
     def calc_height(self, n=None):
         if n is None: return 0
         return 1 + max(self.calc_height(n.left), self.calc_height(n.right))
 
-
-#print_result
-def print_result(tree, search_list):
-    print("사전 파일을 모두 읽었습니다. %d개의 단어가 있습니다." % tree.get_total_count(tree.root))
-    print("트리의 전체 높이는 %d 입니다." % tree.calc_height(treeA.root))
-    print("랜덤하게 선택된 단어 %d개 : " % len(search_list), end="")
-    for word in search_list:
-        print(word, end=" ")
-    print()
     
-    for word in search_list:
-        result, level = tree.search(word)
-        print("%s %s (레벨 %d)" % (result.item[0], result.item[1], level))
 
+f = [["apple"], ["zebra"], ["banana"], ["water"], ["game"]]
 
-#read word list
-with open('./assignment5/randdict_utf8.TXT', 'r', encoding="utf-8") as f:  
-    
-    i = 0
-    treeA = TreeA()
-    treeB = TreeB()
-    word_list = list()
+treeA = TreeA()
+treeB = TreeB()
 
-    for line in f:
-        line = line.strip().split(":")
-        line = list(map(lambda x: x.strip(), line))
+i = 0
+for line in f:
+    #형식에 맞지 않으면 randdict에 추가하지 않는다.
+    if line[0] == "":
+        continue
+    else:
+        treeA.insert(line)
+        treeB.insert(line)
+        print(line)
 
-        #형식에 맞지 않으면 randdict에 추가하지 않는다.
-        if line[1] == "":
-            continue
-        else:
-            treeA.insert(line)
-            treeB.insert(line)
-            word_list.append(line[0])
-            #print(line)
-
-        if i == 10:
-            break
-        else:
-            i += 1
-
-
+print()
 treeA.print(treeA.root)
+print()
+treeB.print(treeB.root)
 
-# print result
-DATA_SIZE = 3
-search_list = random.sample(word_list, DATA_SIZE)
-print_result(treeA, search_list)
+print()
+treeA.print_pre(treeA.root)
+print()
+treeB.print_pre(treeB.root)
 
+print("\n\n")
+print(treeA.calc_height(treeA.root))
+print(treeB.calc_height(treeB.root))
 
+    

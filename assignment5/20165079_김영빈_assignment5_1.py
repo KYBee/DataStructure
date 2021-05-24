41c543c765c8b758d9d6090709d76f5c4a4807e7
KYBee <kyb3634@naver.com>
2021-05-25 03:14:44 +0900

    add comments

--- assignment5/20165079_김영빈_assignment5_1.py
+++ assignment5/20165079_김영빈_assignment5_1.py
@@ -31,15 +31,18 @@ class Node :
 
 class Tree :
     def __init__(self):
+        #tree의 root 노드를 pointing 할 변수
         self.root = None
 
+    #preorder를 위한 class method
     @classmethod
     def preorder(cls, n):
         if n != None :
             print(n.item,end=" ")
             cls.preorder(n.left)
             cls.preorder(n.right)
-        
+    
+    #inorder를 위한 class method
     @classmethod
     def inorder(cls, n):
         if n != None :
@@ -47,6 +50,7 @@ class Tree :
             print(n.item, end=" ")
             cls.inorder(n.right)
     
+    #postorder를 위한 class method
     @classmethod
     def postorder(cls, n):
         if n != None :
@@ -54,6 +58,7 @@ class Tree :
             cls.postorder(n.right)
             print(n.item, end=" ")
 
+    #levelorder를 위한 class method
     @classmethod
     def levelorder(cls, n):
         if n != None:
@@ -66,16 +71,23 @@ class Tree :
                     queue.enqueue(n.left)
                     queue.enqueue(n.right)
         
+#위에서 구현한 tree를 이용하여 만든 계산기 class
 class Calculator :
     def __init__(self):
+        #사용할 tree 인스턴스 변수
         self.tree = Tree()
+        #사용자에게 입력받은 식을 저장할 인스턴스 변수
         self.equation = None
+        #operator weight를 계산할 토큰
         self.operator = "+-*/"
+        #식의 값을 저장할 인스턴스 변수
         self.value = 0
 
+    #사용자에게 식을 입력받는 메서드
     def get_equation(self):
         self.equation = input("수식을 입력하세요 : ").strip()
 
+    #사용자에게 입력받은 식으로 트리를 구성하는 메서드
     def get_tree(self):
         i = 1
         while i < len(self.equation) - 1:
@@ -88,11 +100,13 @@ class Calculator :
                 self.tree.root = self.get_sub_tree(self.tree.root, Node(self.equation[i]), Node(self.equation[i + 1]))
             i += 2
 
+    #2개의 피연산자와 1개의 연산자를 이용하여 서브 트리를 만드는 메서드
     def get_sub_tree(self, left, root, right):
         root.left = left
         root.right = right
         return root
 
+    #결과를 프린트해주는 메서드
     def print_result(self):
         print("전위 순회 : ", end="")
         self.tree.preorder(self.tree.root)
@@ -104,15 +118,17 @@ class Calculator :
         self.tree.levelorder(self.tree.root)
         self.get_value()
 
+    #결과값을 출력하고 초기화 하는 메서드
     def get_value(self):
         self.value = self.postorder_calculate(self.tree.root)
         print("\n계산 결과 :", self.value)
 
-        #initialize
+        #Calculator의 run 메서드를 무한반복으로 실행했기 때문에 다음 run이 실행되기 전에 3개의 인스턴스 변수를 초기화 시켜줌
         self.value = 0
         self.equation = None
         self.tree.root = None
 
+    #후위 계산으로 결과 값을 구하는 메서드
     def postorder_calculate(self, n):
         if n != None :
             left = self.postorder_calculate(n.left)
@@ -130,11 +146,12 @@ class Calculator :
             else:
                 return int(op)
 
+    #계산기를 실행하는 메서드
     def run(self):
         self.get_equation()
         self.get_tree()
         self.print_result()
-        
+
 c = Calculator()
 while True:
     c.run()
--- assignment5/20165079_김영빈_assignment5_2.py
+++ assignment5/20165079_김영빈_assignment5_2.py
@@ -9,14 +9,13 @@ class Node :
     def __str__(self):
         return self.item
 
+#이진 탐색 트리
 class TreeA :
     def __init__(self):
         self.root = None
 
-    def get_total_count(self, n):
-        if n is None: return 0
-        return 1 + self.get_total_count(n.left) + self.get_total_count(n.right)
-
+    #중위 순위를 하면서 트리의 모든 단어를 정렬된 상태로 출력해주는 메서드
+    #디버깅을 위해서 구현
     @classmethod
     def print(cls, n):
         if n != None :
@@ -24,12 +23,14 @@ class TreeA :
             print(n.item)
             cls.print(n.right)
 
+    #입력받은 item을 트리에서 찾아서 돌려주는 탐색 메서드
     def search(self, item):
         if self.root is None:
             return None
         else:
             return self.__search_node(self.root, item, level = 0)
 
+    #search메서드에서 단어를 탐색하는데 사용하는 메서드
     def __search_node(self, cur, item, level):
         if cur is None:
             return None, None
@@ -43,39 +44,46 @@ class TreeA :
                 level += 1
                 return self.__search_node(cur.right, item, level)
 
+    #item을 트리에 이진 탐색 트리에 특성에 맞도록 추가하는 메서드
     def insert(self, item):
         if self.root is None:
             self.root = Node(item)
         else:
             self.__insert_node(self.root, item)
-        
+    
+    #insert메서드에서 단어를 추가하는데 사용하는 메서드
     def __insert_node(self, cur, item):
-        #head 값이 크면 왼쪽으로
+        #root에 있는 단어의 값이 크면 왼쪽으로
         if cur.item[0] >= item[0]:
             if cur.left is not None:
                 self.__insert_node(cur.left, item)
             else:
                 cur.left = Node(item)
-        #head 값이 작으면 오른쪽으로
+        #root에 있는 단어의 값이 작으면 오른쪽으로
         else:
             if cur.right is not None:
                 self.__insert_node(cur.right, item)
             else:
                 cur.right = Node(item)
-        
+    
+    #트리의 높이를 구하는 메서드
     def calc_height(self, n=None):
         if n is None: return 0
         return 1 + max(self.calc_height(n.left), self.calc_height(n.right))
 
-
-class TreeB :
-    def __init__(self):
-        self.root = None
-
+    # 총 몇개의 노드가 있는지를 구하는 메서드
     def get_total_count(self, n):
         if n is None: return 0
         return 1 + self.get_total_count(n.left) + self.get_total_count(n.right)
 
+
+#스스로 균형을 잡는 이진 탐색 트리
+class TreeB :
+    def __init__(self):
+        self.root = None
+
+    #중위 순위를 하면서 트리의 모든 단어를 정렬된 상태로 출력해주는 메서드
+    #디버깅을 위해서 구현
     @classmethod
     def print(cls, n):
         if n != None :
@@ -83,12 +91,14 @@ class TreeB :
             print(n.item, end=" ")
             cls.print(n.right)
 
+    #입력받은 item을 트리에서 찾아서 돌려주는 탐색 메서드
     def search(self, item):
         if self.root is None:
             return None
         else:
             return self.__search_node(self.root, item, level = 0)
 
+    #search메서드에서 단어를 탐색하는데 사용하는 메서드
     def __search_node(self, cur, item, level):
         if cur is None:
             return None, None
@@ -102,28 +112,32 @@ class TreeB :
                 level += 1
                 return self.__search_node(cur.right, item, level)
 
+    #item을 트리에 이진 탐색 트리에 특성에 맞도록 추가하는 메서드
     def insert(self, item):
         if self.root is None:
             self.root = Node(item)
         else:
             self.root = self.__insert_node(self.root, item)
-        
+
+    #insert메서드에서 단어를 추가하는데 사용하는 메서드
     def __insert_node(self, cur, item):
-        #head 값이 크면 왼쪽으로
+        #root에 있는 단어의 값이 크면 왼쪽으로
         if cur.item[0] >= item[0]:
             if cur.left is not None:
                 cur.left = self.__insert_node(cur.left, item)
             else:
                 cur.left = Node(item)
-        #head 값이 작으면 오른쪽으로
+        #root에 있는 단어의 값이 작으면 오른쪽으로
         else:
             if cur.right is not None:
                 cur.right = self.__insert_node(cur.right, item)
             else:
                 cur.right = Node(item)
 
+        #만약의 트리가 편향 트리가 되었다면 다시 균형을 맞추어주기 위해 실행한다.
         return self.rebalance(cur)
 
+    #트리의 균형을 맞추기 위한 메서드
     def rebalance(self, cur):
 
         left_height = self.calc_height(cur.left)
@@ -163,12 +177,18 @@ class TreeB :
         cur.left = self.rrrotate(B)
         return self.llrotate(cur)
 
+    #트리의 높이를 구하는 메서드
     def calc_height(self, n=None):
         if n is None: return 0
         return 1 + max(self.calc_height(n.left), self.calc_height(n.right))
+    
+    # 총 몇개의 노드가 있는지를 구하는 메서드
+    def get_total_count(self, n):
+        if n is None: return 0
+        return 1 + self.get_total_count(n.left) + self.get_total_count(n.right)
 
 
-#print_result
+#결과를 출력하는 함수
 def print_result(tree, search_list):
     print("사전 파일을 모두 읽었습니다. %d개의 단어가 있습니다." % tree.get_total_count(tree.root))
     print("트리의 전체 높이는 %d 입니다." % tree.calc_height(tree.root))
@@ -181,32 +201,40 @@ def print_result(tree, search_list):
         result, level = tree.search(word)
         print("%s %s (레벨 %d)" % (result.item[0], result.item[1], level))
 
+#몇 개의 단어를 랜덤하게 뽑을지 지정하는 변수
 DATA_SIZE = 10
 
-#read word list
-#TODO need to change directory
-with open('./assignment5/randdict_utf8.TXT', 'r', encoding="utf-8") as f:  
+#파일을 열어서 단어를 읽어온다.
+with open('./randdict_utf8.TXT', 'r', encoding="utf-8") as f:  
     
-    i = 0
+    # i 는 디버깅을 위한 변수
+    #i = 0
     treeA = TreeA()
     treeB = TreeB()
+    
+    #word_list는 단어를 랜덤하게 선택하기 위해 저장해두는 리스트
     word_list = list()
 
     for line in f:
         line = line.strip().split(":")
         line = list(map(lambda x: x.strip(), line))
 
-        #형식에 맞지 않으면 randdict에 추가하지 않는다.
+        #형식에 맞지 않으면 tree와 전체 word_list에 추가하지 않는다.
         if line[1] == "":
             continue
         else:
             treeA.insert(line)
             treeB.insert(line)
             word_list.append(line[0])
-            print(line)
+            
+            #아래 2줄은 디버깅을 위한 코드
+            #print(i, line)
+            #i+=1
 
-# print result
+#랜덤으로 DATA_SIZE만큼의 단어를 선택함
 search_list = random.sample(word_list, DATA_SIZE)
+
+#treeA와 treeB의 결과 출력
 print_result(treeA, search_list)
 print()
 print_result(treeB, search_list)

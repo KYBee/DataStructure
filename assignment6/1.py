76a59d5b7379c80880aed2db06a2d985c78676a7
KYBee <kyb3634@naver.com>
2021-06-06 16:49:04 +0900

    implementing get_distance

--- assignment6/1.py
+++ assignment6/1.py
@@ -3,6 +3,7 @@ import collections
 class Station:
     def __init__(self, name):
         self.name=name
+        #Dictionary로 구성되어 있으므로 따로 Sorting 할 필요가 없음
         self.adjacent_stations = {}
 
     def __str__(self):
@@ -11,18 +12,15 @@ class Station:
 class Map:
     def __init__(self):
         self.stations = None
-        self.distance_map = None
 
     def set_map(self, stations, connections):
         self.stations = stations
         self.connect_stations(connections)
-        self.distance_map = self.get_distance_map()
 
     def get_available_station(self):
         return self.stations.values()
 
     def connect_stations(self, connections):
-
         for connection in connections:
             if len(connection) != 3:
                 continue
@@ -32,9 +30,59 @@ class Map:
             self.stations[station1].adjacent_stations[station2] = weight
             self.stations[station2].adjacent_stations[station1] = weight
 
-    def get_distance_map(self):
-        pass
+    def get_distance(self, start, end):
+        distance = {}
+        final_distance = {}
+        final_distance[start] = [0, start]
 
+        #initialize
+        cur = self.stations[start]
+        for station in cur.adjacent_stations.keys():
+            distance[station] = [cur.adjacent_stations[station], cur.name]
+
+        nearest, weight = sorted(distance.items(), key=lambda item: item[1])[0]
+        del distance[nearest]
+        final_distance[nearest] = weight
+
+
+        for i in range(30):
+        #while True:
+            #가장 작은 weight를 가진 역을 하나 뽑아서 target으로 설정한다.
+            
+            cur = self.stations[nearest]
+
+            for station in cur.adjacent_stations.keys():
+                if station in final_distance:
+                    continue
+                elif station in distance.keys():
+                    weight = [final_distance[cur.name][0] + cur.adjacent_stations[station], cur.name]
+                    temp = distance[station]
+
+                    if weight[0] < temp[0]:
+                        distance[station] = weight
+
+                else:
+                    distance[station] = [cur.adjacent_stations[station], cur.name]
+
+            print("distance 한 번 끝")
+            print(distance)
+            print(final_distance)
+            print()
+            print(cur)
+            print()
+
+            nearest, weight = sorted(distance.items(), key=lambda item: item[1])[0]
+            del distance[nearest]
+            final_distance[nearest] = weight
+            print("final distance 한 번 끝")
+            print(distance)
+            print(final_distance)
+            print()
+            print()
+            print()
+
+        return final_distance
+        
 
 
 all_stations = {
@@ -47,36 +95,12 @@ all_stations = {
 }
 
 all_distance = [
-    ['가양', '당산', 6],
-    ['당산', '노량진', 4],
-    ['노량진', '흑석', 2],
-    ['흑석', '동작', 1],
-    ['동작', '고속터미널', 3],
-    ['고속터미널', '이수', 2],
-    ['이수', '상도', 3],
-    ['상도', '대림', 5],
-    ['대림', '가산디지털단지', 2],
-    ['가산디지털단지', '광명사거리', 2],
-    ['당산', '문래', 2],
-    ['문래', '신도림', 1],
-    ['신도림', '대림', 1],
-    ['대림', '서울대입구', 5],
-    ['서울대입구', '사당', 2],
-    ['사당', '서초', 2],
-    ['성균관대', '금정', 4],
-    ['금정', '안양', 2],
-    ['안양', '가산디지털단지', 5],
-    ['가산디지털단지', '신도림', 2],
-    ['신도림', '노량진', 4],
-    ['노량진', '동대문', 8],
-    ['중앙', '금정', 7],
-    ['금정', '인덕원', 4],
-    ['인덕원', '과천', 2],
-    ['과천', '사당', 5],
-    ['사당', '이수', 1],
-    ['이수','동작', 1],
-    ['동작', "동대문", 10],
-    ['동대문', "당고개", 8],
+    ['가양', '당산', 6], ['당산', '노량진', 4], ['노량진', '흑석', 2], ['흑석', '동작', 1], ['동작', '고속터미널', 3],
+    ['고속터미널', '이수', 2], ['이수', '상도', 3], ['상도', '대림', 5], ['대림', '가산디지털단지', 2], ['가산디지털단지', '광명사거리', 2],
+    ['당산', '문래', 2], ['문래', '신도림', 1], ['신도림', '대림', 1], ['대림', '서울대입구', 5], ['서울대입구', '사당', 2],
+    ['사당', '서초', 2], ['성균관대', '금정', 4], ['금정', '안양', 2], ['안양', '가산디지털단지', 5], ['가산디지털단지', '신도림', 2],
+    ['신도림', '노량진', 4], ['노량진', '동대문', 8], ['중앙', '금정', 7], ['금정', '인덕원', 4], ['인덕원', '과천', 2],
+    ['과천', '사당', 5], ['사당', '이수', 1], ['이수','동작', 1], ['동작', "동대문", 10], ['동대문', "당고개", 9],
 ]
 
 Seoul_Train = Map()
@@ -85,3 +109,5 @@ Seoul_Train.set_map(all_stations, all_distance)
 for i in Seoul_Train.get_available_station():
     print(i, end=" ")
     print(i.adjacent_stations)
+
+Seoul_Train.get_distance("중앙", "대림")
